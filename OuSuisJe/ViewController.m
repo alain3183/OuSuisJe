c1603a58dd2f30681b751099fbcb05f72af30551
Alain Malenfant <alainmalenfant@yahoo.com>
2013-11-10 19:13:24 -0500

    Add VC.m

--- OuSuisJe/ViewController.m
+++ OuSuisJe/ViewController.m
@@ -10,14 +10,41 @@
 
 @interface ViewController ()
 
+
+
 @end
 
 @implementation ViewController
+
+- (void) locationManager: (CLLocationManager *)manager didUpdateToLocation:(CLLocation *)newLocation fromLocation:(CLLocation *)oldLocation
+{
+    _maPosition.text = [newLocation description];
+}
+
+- (void) locationManager: (CLLocationManager *)manager didFailWithError:(NSError *)error
+{
+    _maPosition.text = [error description];
+}
+
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    // initialisation de l'objet locationManager
+    locationManager = [[CLLocationManager alloc]init];
+    // pour voir si le service de géolocalisation est disponible et activé. Ex. en mode "Avion" est activé ou si les périphériques de géolocalisation ne sont pas en mesure de fournir des informations, il est inutile de chercher à en obtenir
+    if ([CLLocationManager locationServicesEnabled])
+    {
+        // affecte la valeur self à la propriété delegate de l'objet locationManager afin d'indiquer que les événements relatifs à la géolocalisation doivent être traités dans VC.m
+        locationManager.delegate = self;
+        // précise la précision désirée
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+        // définit la distance de déplacement minimale du device avant qu'une MAJ de la position ne soit effectuée, il faudra que le device se déplace de 100 mètres pour qu'une fotification de changement ne soit faite
+        locationManager.distanceFilter = 100.0f;
+        // éxécute la méthode startUpdatingLocation, en d'autres termes, elle demande au device de MAJ de façon régulière ses coordonnées géographiques.
+        [locationManager startUpdatingLocation];
+    }
 }
 
 - (void)didReceiveMemoryWarning
